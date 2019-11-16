1704411ab4981b9a0af3d3c763dc7e0698463e7e
Shekhar Wagh <shekhar.wagh@team.telstra.com>
2019-11-16 21:50:35 +0530

    Updates

---
+++ precache-manifest.8af343157b8559dc44ffe5d5ec08a456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557924ed734d44f39ea897c12236c950",
+    "url": "/live-stocks-app/index.html"
+  },
+  {
+    "revision": "b827b79bcb85777bfdc6",
+    "url": "/live-stocks-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d8cd2364c9774008e7e7",
+    "url": "/live-stocks-app/static/js/2.7fea46ba.chunk.js"
+  },
+  {
+    "revision": "b827b79bcb85777bfdc6",
+    "url": "/live-stocks-app/static/js/main.fd2483f7.chunk.js"
+  },
+  {
+    "revision": "f6f66b87bd87d3b7ebd1",
+    "url": "/live-stocks-app/static/js/runtime-main.c0e2e6c1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/live-stocks-app/static/media/logo.25bf045c.svg"
+  }
+]);
